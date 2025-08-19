dabb0649981b98bab8adea95d4ff82c4f12452b5
Herik Colares Rezende <herik.rezende@pedreiraumvalemix.com.br>
2025-08-19 17:08:06 -0300

    feat: enhance NavBar styling and structure in HomePage component

--- src/presentation/home/components/navbar.tsx
+++ src/presentation/home/components/navbar.tsx
@@ -1,6 +1,16 @@
 export default function NavBarComp() {
   return (
-    <nav style={{ width: "50vw" }} className="flex justify-center items-center p-4 bg-[var(--primary-color)]">
+    <nav
+      style={{
+        width: "50vw",
+        border: "1px solid var(--grey-color)",
+        backgroundColor: "var(--grey-color)",
+        borderRadius: "8px",
+        padding: "1rem 2rem",
+        height: "auto"
+      }}
+      className="flex justify-center items-center p-4"
+    >
       <ul className="flex space-x-4">
         <li>
           <a href="#" className="text-black hover:underline">
--- src/presentation/home/page.tsx
+++ src/presentation/home/page.tsx
@@ -6,13 +6,20 @@ import style from "./style.module.css";
 export default function HomePage() {
   return (
     <SlideInEffect duration={1}>
-      <header className="flex flex-row fixed top-0 left-0 w-full h-16 bg-[var(--primary-color)] justify-between items-center px-4">
-        <picture>
-            <img src={logoP1} alt="Logo P1" className="h-12" style={{ width: "10vw", objectFit: "contain" }} />
-        </picture>
-        <NavBarComp />
-        <button className={style.novo_chamado_btn}>Novo Chamado</button>
-      </header>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <header className="flex flex-row fixed top-0 left-0 w-full h-16 bg-[var(--primary-color)] justify-between items-center px-4 ">
+          <picture>
+            <img
+              src={logoP1}
+              alt="Logo P1"
+              className="h-12"
+              style={{ width: "10vw", objectFit: "contain" }}
+            />
+          </picture>
+          <NavBarComp />
+          <button className={style.novo_chamado_btn}>Novo Chamado</button>
+        </header>
+      </div>
     </SlideInEffect>
   );
 }
